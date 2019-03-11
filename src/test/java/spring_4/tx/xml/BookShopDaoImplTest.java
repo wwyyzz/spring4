ae2ac8826b68fe7a0ad5b04fa2337ef455ea7ed8
wwyyzz <sdn_app@163.com>
2019-03-11 17:13:35 +0800

    tx-xml

--- src/test/java/spring_4/tx/xml/BookShopDaoImplTest.java
+++ src/test/java/spring_4/tx/xml/BookShopDaoImplTest.java
@@ -18,16 +18,16 @@ public class BookShopDaoImplTest {
 
     @Test
     public void testfindBookPriceBIsbn() {
-        System.out.println(bookShopDao.findBookPriceBIsbn("1001"));
+        System.out.println(bookShopDao.findBookPriceBIsbn("0001"));
     }
 
     @Test
     public void testupdateBookStore() {
-        bookShopDao.updateBookStore("1001");
+        bookShopDao.updateBookStore("0001");
     }
 
     @Test
     public void testupdateUserAccount() {
-        bookShopDao.updateUserAccount("AA", 100);
+        bookShopDao.updateUserAccount("Mike", 10);
     }
 }
